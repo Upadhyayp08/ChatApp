bfa0c65aa260335982ba642c80ea7ccaac9de06e
Parthwedev <upadhyayparth080@gmail.com>
2023-07-20 21:36:16 +0530

    Update in link
    Update in link

--- Frontend/src/components/ChatArea.js
+++ Frontend/src/components/ChatArea.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://funny-complete-anchovy.glitch.me");
 
 const ChatArea = () => {
   // Creating state to handle different values
